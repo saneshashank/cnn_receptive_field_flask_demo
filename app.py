5365c312381c69468e4c2bc06bdf35425632f273
shashank <sane.shashank@gmail.com>
2025-08-24 16:56:32 +0530

    added code for port 5000

--- app.py
+++ app.py
@@ -8,5 +8,5 @@ def index():
     return render_template("index.html")  # Single-page UI
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
     
